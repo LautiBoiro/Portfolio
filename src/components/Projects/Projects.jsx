42386736d54565f8d580c814cf87be1c57ff4544
LautiBoiro <lautaroboiro2010@live.com.ar>
2026-06-28 20:56:07 -0300

    fix responsive and added new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,15 +10,16 @@ const PROJECTS = [ // para agregar más simplemente coma luego del } y repetir e
     github: 'https://github.com/LautiBoiro/Who-wants-to-be-a-millionaire-Pygame',
     demo: null,
     image: '/projects/millionaire.png'
-  },/*
+  },
   {
     id: 1,
-    title: 'Nada',
-    description: 'Descripción.',
-    tags: ['React', 'CSS', 'JavaScript'],
-    github: 'https://github.com',
-    demo: null
-  }*/
+    title: 'Burger ecommerce',
+    description: 'React-based e-commerce web application that simulates an online burger shop with shopping cart functionality, product management, and responsive UI design.',
+    tags: ['React', 'CSS', 'JavaScript', 'PHP'],
+    github: 'https://github.com/LautiBoiro/burger-ecommerce.git',
+    demo: null,
+    image: '/projects/burger.png'
+  }
 ]
 
 export function Projects () {
